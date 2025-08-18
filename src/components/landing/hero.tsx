427d2ffb2eed05f6c4da74cbd9f4084bdf17f34c
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-18 18:32:06 +0000

    Develop, Build, Manage, and scale your .......... business all in one te

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -3,18 +3,18 @@ import { ArrowRight } from 'lucide-react';
 import AnimatedText from './animated-text';
 
 export default function Hero() {
-  const words = ["dApp", "token", "trading bot", "custom wallet", "smart contract", "Web3 project"];
+  const words = ["Web3 project", "dApp", "token", "trading bot", "custom wallet", "smart contract"];
   
   return (
     <section className="bg-background text-foreground">
       <div className="container mx-auto px-4 h-screen flex flex-col justify-center items-center">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight font-headline mb-10">
-            The ultimate terminal to build your next <AnimatedText words={words} className="text-primary" />
+            Build, manage, and grow your <AnimatedText words={words} className="text-primary" /> with the all-in-one text-to-code platform.
           </h1>
           <div className="flex justify-center items-center gap-4 mt-20">
             <Button size="lg">
-              Start Building <ArrowRight className="ml-2 h-5 w-5" />
+              Start for Free <ArrowRight className="ml-2 h-5 w-5" />
             </Button>
           </div>
         </div>
