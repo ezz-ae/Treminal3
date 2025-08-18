ee41fafa56969f3fa3b799b58c5253b4d29d4190
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-18 18:35:04 +0000

    we also can play same game with build develop manage create grow scale p

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -3,14 +3,15 @@ import { ArrowRight } from 'lucide-react';
 import AnimatedText from './animated-text';
 
 export default function Hero() {
-  const words = ["Web3 project", "dApp", "token", "trading bot", "custom wallet", "smart contract"];
+  const actionWords = ["Build", "Develop", "Manage", "Create", "Grow", "Scale", "Plan", "Start"];
   
   return (
     <section className="bg-background text-foreground">
       <div className="container mx-auto px-4 h-screen flex flex-col justify-center items-center">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight font-headline mb-10">
-            Build, manage, and grow your <AnimatedText words={words} className="text-primary inline-block" /> with the all-in-one text-to-code platform.
+            <AnimatedText words={actionWords} className="text-primary inline-block mr-3" />
+            any Web3 project at any scale with the all-in-one text-to-code platform.
           </h1>
           <div className="flex justify-center items-center gap-4 mt-20">
             <Button size="lg">
