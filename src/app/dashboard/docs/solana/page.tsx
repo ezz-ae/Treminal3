9671f91c8c0c79f6f4f097d8d9d6b8715b4b9f0e
Mahmoud Ezz <Mu7moud@gmail.com>
2025-09-04 21:53:15 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 're

--- src/app/dashboard/docs/solana/page.tsx
+++ src/app/dashboard/docs/solana/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Button } from '@/components/ui/button';
 import { Copy, BotMessageSquare } from 'lucide-react';
 import { useToast } from '@/hooks/use-toast';
@@ -131,5 +131,3 @@ fetch(url, {
     </div>
   )
 }
-
-    
