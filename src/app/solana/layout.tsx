9f98f8c4be79e7e8ec6c31601cfec8d26a53b768
Mahmoud Ezz <Mu7moud@gmail.com>
2025-10-22 23:42:37 +0000

    now go to solana --- solana page itself has internal header remove it

--- src/app/solana/layout.tsx
+++ src/app/solana/layout.tsx
@@ -1,12 +1,10 @@
 'use client';
 import { ReactNode } from 'react';
-import SolanaHeader from '@/components/layout/solana-header';
 
 export default function SolanaLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <SolanaHeader />
-      <main className="flex-1 bg-black">
+      <main className="flex-1 flex flex-col bg-black">
         {children}
       </main>
     </>
